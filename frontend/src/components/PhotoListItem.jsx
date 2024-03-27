39670cd65dd1c7efcb4b86ef34675579d5992672
LiamGunning07 <liamgunning07@gmail.com>
2024-03-27 15:33:59 -0600

    Render component with props from app.jsx

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -2,31 +2,17 @@ import React from "react";
 
 import "../styles/PhotoListItem.scss";
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
-
-
 const PhotoListItem = (props) => {
 
-const { id, location, imageSource, username, profile } = sampleDataForPhotoListItem
+const { id, location, imageSource, username, profile } = props
 
   return (
     <div className="photo-list-item">
-      <img src={imageSource} alt="Photo" className="photo-image" />
-      <div className="photo-details">
-        <h2 className="photo-username">{username}</h2>
-        <p className="photo-location">{`${location.city}, ${location.country}`}</p>
-        <p className="photo-id">{id}</p>
-      </div>
-      <img src={profile} alt="Profile" className="profile-image" />
+      <img src={props.imageSource} alt="Photo" className="photo-image" />
+        <img src={props.profile} alt="Profile" className="profile-image" />
+        <h2 className="photo-username">{props.username}</h2>
+        <p className="photo-location">{`${props.location.city}, ${props.location.country}`}</p>
+        <p className="photo-id" key={id}>{id}</p>
     </div>
   )
 };
