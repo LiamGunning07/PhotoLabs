7ed632fedf9788c673de3bedb620ca6a2fe88801
LiamGunning07 <liamgunning07@gmail.com>
2024-03-27 16:13:25 -0600

    Changed class names to apply styling

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,12 +6,12 @@ const PhotoListItem = (props) => {
 
 
   return (
-    <div className="photo-list-item">
-      <img src={props.photo.imageSource} alt="Photo" className="photo-image" />
-      <img src={props.photo.profile} alt="Profile" className="profile-image" />
-      <h2 className="photo-username">{props.photo.username}</h2>
-      <p className="photo-location">{`${props.photo.location.city}, ${props.photo.location.country}`}</p>
-      <p className="photo-id" key={props.photo.id}>{props.photo.id}</p>
+    <div className="photo-list__item">
+      <img src={props.photo.imageSource} alt="Photo" className="photo-list__image" />
+      <img src={props.photo.profile} alt="Profile" className="photo-list__user-profile" />
+      <h2 className="photo-list__user-details">{props.photo.username}</h2>
+      <p className="photo-list__user-location">{`${props.photo.location.city}, ${props.photo.location.country}`}</p>
+      <p className="photo-list__user-info" key={props.photo.id}>{props.photo.id}</p>
     </div>
   )
 };
