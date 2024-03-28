97a476f41bc74e91c89e6f03c3339467b2d3b6e0
LiamGunning07 <liamgunning07@gmail.com>
2024-03-28 16:19:46 -0600

    Added fav button into the list item

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -1,16 +1,25 @@
 import React from "react";
-
+import PhotoFavButton from "./PhotoFavButton";
 import "../styles/PhotoListItem.scss";
+import { useState } from "react";
 
 const PhotoListItem = (props) => {
+  const [ isFavourite , setIsFavourite] = useState(false)
 
+  const handleFavouriteClick = () => {
+    console.log('Fav clicked')
+    setIsFavourite(prevState => !prevState); // Toggle the favorite status
+  };
 
   return (
   <div className="photo-list__item">
+    <div>
+      <PhotoFavButton className='photo-list__fav-icon-svg' isFavourite={isFavourite} onClick={handleFavouriteClick} />
+    </div>
     <img src={props.photo.imageSource} alt="Photo" className="photo-list__image" />
       <div className="photo-list__user-details">
         <img src={props.photo.profile} alt="Profile" className="photo-list__user-profile" />
-        <h2 className="photo-list__username">{props.photo.username}</h2>
+        <div className="photo-list__username">{props.photo.username}</div>
         <p className="photo-list__user-location">{`${props.photo.location.city}, ${props.photo.location.country}`}</p>
       </div>
   </div>
