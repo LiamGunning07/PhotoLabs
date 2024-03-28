1b7c76b7fbdc39897eaf9fdd6fac52d35963cfdb
LiamGunning07 <liamgunning07@gmail.com>
2024-03-28 16:19:12 -0600

    Added like button with state and handler

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
 
-
 const sampleDataForPhotoListItem = {
   id: "1",
   location: {
--- frontend/src/components/PhotoFavButton.jsx
+++ frontend/src/components/PhotoFavButton.jsx
@@ -1,13 +1,19 @@
 import React, { useCallback, useState } from 'react';
-
 import FavIcon from './FavIcon';
 import '../styles/PhotoFavButton.scss';
 
+
 function PhotoFavButton() {
+  const [ isFavourite , setIsFavourite] = useState(false)
+
+  const handleFavouriteClick = () => {
+    setIsFavourite(prevState => !prevState); // Toggle the favourite status
+  };
+
   return (
     <div className="photo-list__fav-icon">
       <div className="photo-list__fav-icon-svg">
-        {/* Insert React */}
+        <FavIcon isFavourite={isFavourite} onClick={handleFavouriteClick} />
       </div>
     </div>
   );
