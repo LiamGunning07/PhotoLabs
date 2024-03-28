d43c4407e5c151863bbc8df27a21aa634c03c1e6
LiamGunning07 <liamgunning07@gmail.com>
2024-03-28 15:22:22 -0600

    Add New longer array containing sample datat for more posts

--- frontend/src/components/PhotoListItem.jsx
+++ frontend/src/components/PhotoListItem.jsx
@@ -6,13 +6,14 @@ const PhotoListItem = (props) => {
 
 
   return (
-    <div className="photo-list__item">
-      <img src={props.photo.imageSource} alt="Photo" className="photo-list__image" />
-      <img src={props.photo.profile} alt="Profile" className="photo-list__user-profile" />
-      <h2 className="photo-list__user-info">{props.photo.username}</h2>
-      <p className="photo-list__user-location">{`${props.photo.location.city}, ${props.photo.location.country}`}</p>
-      <p className="photo-list__user-info" key={props.photo.id}>{props.photo.id}</p>
-    </div>
+  <div className="photo-list__item">
+    <img src={props.photo.imageSource} alt="Photo" className="photo-list__image" />
+      <div className="photo-list__user-details">
+        <img src={props.photo.profile} alt="Profile" className="photo-list__user-profile" />
+        <h2 className="photo-list__username">{props.photo.username}</h2>
+        <p className="photo-list__user-location">{`${props.photo.location.city}, ${props.photo.location.country}`}</p>
+      </div>
+  </div>
   )
 };
 
