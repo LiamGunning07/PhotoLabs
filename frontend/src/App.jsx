fed15a2ed1ad71c710b089371e8107896565f85c
LiamGunning07 <liamgunning07@gmail.com>
2024-03-29 12:33:43 -0600

    Add PhotoList

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,27 +1,19 @@
 import React from 'react';
 import PhotoListItem from './components/PhotoListItem';
 import './App.scss';
+import PhotoList from 'components/PhotoList';
 
-const sampleDataForPhotoListItem = {
-  id: "1",
-  location: {
-    city: "Montreal",
-    country: "Canada",
-  },
-  imageSource: `${process.env.PUBLIC_URL}/Image-1-Regular.jpeg`,
-  username: "Joe Example",
-  profile: `${process.env.PUBLIC_URL}/profile-1.jpg`,
-};
 
 // Note: Rendering a single component to build components in isolation
 const App = () => {
-  const photos = [...Array(3)]; // Builds a new array with 3 posts filled with same sampledata
+//   const photos = [...Array(3)]; // Builds a new array with 3 posts filled with same sampledata
 
   return (
     <div className="App">
-      {photos.map((photo, i) => 
+      {/* {photos.map((photo, i) => 
         <PhotoListItem key={i} photo={sampleDataForPhotoListItem} />
-      )}
+      )} */}
+      <PhotoList />
     </div>
   );
 };
