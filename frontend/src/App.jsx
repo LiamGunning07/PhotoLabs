e3c2f0c177fde48f6bb5f1531c244a8f8a42f259
LiamGunning07 <liamgunning07@gmail.com>
2024-04-01 14:04:07 -0600

    Implemented mock data

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,5 @@
 import React from 'react';
 import './App.scss';
-import PhotoList from 'components/PhotoList';
-import TopNavigationBar from 'components/TopNavigationBar';
-import { useState } from 'react'; // Added { useState }
 import HomeRoute from 'routes/HomeRoute';
 
 // Note: Rendering a single component to build components in isolation
--- frontend/src/components/TopicList.jsx
+++ frontend/src/components/TopicList.jsx
@@ -1,30 +1,13 @@
 import React from "react";
-
+import topics from "mocks/topics";
 import "../styles/TopicList.scss";
 import TopicListItem from "./TopicListItem";
 
-const sampleDataForTopicList = [
-  {
-    id: "1",
-    slug: "topic-1",
-    title: "Nature",
-  },
-  {
-    id: "2",
-    slug: "topic-2",
-    title: "Travel",
-  },
-  {
-    id: "3",
-    slug: "topic-3",
-    title: "People",
-  },
-];
 
 const TopicList = () => {
   return (
     <div className="top-nav-bar__topic-list">
-      {sampleDataForTopicList.map(({id, slug, title}) => (
+      {topics.map(({id, slug, title}) => (
       <TopicListItem title={title} key={id} id={id} slug={slug} /> 
       ))}
     </div>
